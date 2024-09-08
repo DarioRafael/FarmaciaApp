717c116a1e9630c15b2c9400f8352d77bd7593e2
Kawa <dariorafa29@hotmail.com>
2024-09-07 23:43:37 -0600

    Probando #

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
-import 'pages/login_page.dart'; // Asegúrate de que esta importación apunte al archivo correcto
+import 'pages/login_page.dart';
 import 'pages/home_page.dart';
 import 'pages/cobro_page.dart';
 import 'pages/catalogo_page.dart';
 import 'pages/inventario_page.dart';
 import 'pages/reportes_page.dart';
 
-//
 void main() {
-
-
   runApp(const MyApp());
 }
 
@@ -27,94 +24,17 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      // Cambia la ruta inicial a '/login'
       initialRoute: '/login',
-      routes: {
-        '/login': (context) => LoginPage(),
-        '/home': (context) => HomePage(),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/login':
+            return MaterialPageRoute(builder: (_) => LoginPage());
+          case '/home':
+            return MaterialPageRoute(builder: (_) => HomePage());
+          default:
+            return MaterialPageRoute(builder: (_) => LoginPage());
+        }
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-
-  static const List<Widget> _pages = <Widget>[
-    CobroPage(),
-    CatalogoPage(),
-    InventarioPage(),
-    ReportesPage(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Papelería App'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Menú',
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.attach_money),
-              title: const Text('Cobro'),
-              onTap: () {
-                _onItemTapped(0);
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.inventory),
-              title: const Text('Catálogo de Productos'),
-              onTap: () {
-                _onItemTapped(1);
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.storage),
-              title: const Text('Inventario'),
-              onTap: () {
-                _onItemTapped(2);
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.analytics),
-              title: const Text('Reportes'),
-              onTap: () {
-                _onItemTapped(3);
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
-      body: _pages.elementAt(_selectedIndex),
     );
   }
 }
