f24e6394a679e9aa042ab0142f951854452e6946
Dario Rafael <dariorafa29@hotmail.com>
2025-02-25 08:33:52 -0600

    Subir a vercel

--- lib/pages/inventario_page.dart
+++ lib/pages/inventario_page.dart
@@ -53,7 +53,7 @@ class _InventarioPageState extends State<InventarioPage> {
     super.initState();
     _searchController.addListener(_filterProducts);
     _loadProductsFromApi();
-  }//
+  }//Probar de nuevo
 
   Future<void> _loadProductsFromApi() async {
     setState(() {});
